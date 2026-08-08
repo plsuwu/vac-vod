7c8d74b4680cb8edd277c54437d931b51cc85808
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-08-09 04:00:07 +1000

    dataset cleaning cleans subtitle tracks a bit + maps words to timecode. not super robust but it is what it is...

--- flake.nix
+++ flake.nix
@@ -32,35 +32,14 @@
         devShells.default = craneLib.devShell {
           packages = with pkgs; [
             pkg-config
+
             nodejs
             bun
 
             yt-dlp
+            uv
           ];
         };
       }
     );
 }
-
-# {
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#     utils.url = "github:numtide/flake-utils";
-#   };
-#   outputs =
-#     {
-#       self,
-#       nixpkgs,
-#       utils,
-#     }:
-#     utils.lib.eachDefaultSystem (
-#       system:
-#       let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#       in
-#       {
-#         devShell = pkgs.mkShell {
-#         };
-#       }
-#     );
-# }
